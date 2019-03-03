56009dd449a0ef2b583eb16bf8bc6517ef254ecd
voldemort1912 <jaynammodi@gmail.com>
2019-03-03 16:55:47 +0530

    Upgraded to Version 0.3

--- metasploit.sh
+++ metasploit.sh
@@ -4,7 +4,7 @@ cwd=$(pwd)
 name=$(basename "$0")
 export msfinst="$cwd/$name"
 
-msfvar=4.17.37
+msfver=5.0.8
 msfpath='/data/data/com.termux/files/home'
 if [ -d "$msfpath/metasploit-framework" ]; then
 	echo "[#]Deleting Obsolete Files..."
@@ -12,29 +12,35 @@ if [ -d "$msfpath/metasploit-framework" ]; then
 fi
 apt update
 apt install -y autoconf bison clang coreutils curl figlet findutils git apr apr-util libffi-dev libgmp-dev libpcap-dev postgresql-dev readline-dev libsqlite-dev openssl-dev libtool libxml2-dev libxslt-dev ncurses-dev pkg-config wget make ruby-dev libgrpc-dev termux-tools ncurses-utils ncurses unzip zip tar postgresql termux-elf-cleaner ruby
-figlet "MSF!!"
+figlet -c "Metasploit!!"
 echo "
 
 [#] MetaSploit Framework Installer by @hewhomustn0tbenamed (Telegram)!!
 [#] Join @VoldemortCommunity (Telegram) For More!!
 [#] Last Update : 28/02/2019 
 [#] Contact me on Telegram if you Face and Problems!!
-[#] v0.2
+[#] v0.3
 [#] GitHub : https://github.com/VoldemortCommunity
 
 "
 cd $msfpath
-curl -LO https://github.com/rapid7/metasploit-framework/archive/$msfvar.tar.gz
-tar -xf $msfpath/$msfvar.tar.gz
-mv $msfpath/metasploit-framework-$msfvar $msfpath/metasploit-framework
+curl -LO https://github.com/rapid7/metasploit-framework/archive/$msfver.tar.gz
+tar -xf $msfpath/$msfver.tar.gz
+mv $msfpath/metasploit-framework-$msfver $msfpath/metasploit-framework
 cd $msfpath/metasploit-framework
 if [ $(gem list -i rubygems-update) == false ]; then
         gem install rubygems-update
 fi
 
-update_rubygems
-
 gem install bundler
+
+gem install bigdecimal
+gem install pg --version=0.20.0 -- --use-system-libraries
+if [ $(gem list -i rubygems-update) == false ]; then
+        gem install rubygems-update
+fi
+
+update_rubygems
 
 cd $msfpath/metasploit-framework
 bundle install -j5
@@ -74,14 +80,16 @@ pg_ctl -D $PREFIX/var/lib/postgresql start
 createuser msf
 createdb msf_database
 
-rm $msfpath/$msfvar.tar.gz
+rm $msfpath/$msfver.tar.gz
 
 
-cd "$cwd/MetasploitInstaller"
+cd $cwd
 
 bash fix-ruby.sh
 
-echo "[#]You Can directly use msfvenom or msfconsole as they are Symlinked 
-to $PREFIX/bin \n"
-echo "[#]Join @VoldemortCommunity (Telegram) for More!!! \n"
-echo "[#]GitHub : https://GitHub.com/VoldemortCommunity \n "
+echo "
+
+[#]You Can directly use msfvenom or msfconsole as they are Symlinked 
+to $PREFIX/bin
+[#]Join @VoldemortCommunity (Telegram) for More!!!
+[#]GitHub : https://GitHub.com/VoldemortCommunity"
